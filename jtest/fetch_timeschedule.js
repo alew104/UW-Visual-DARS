764554312e1b871058b0948553be6dcff3dafea6
Josh Nazarian <jknaz@cs.washington.edu>
2015-07-18 10:50:16 -0700

    set proper module pattern and onload

--- jtest/fetch_timeschedule.js
+++ jtest/fetch_timeschedule.js
@@ -2,18 +2,24 @@
 // Year:               XXXX (four digits)
 // Departments:        info, cse, art, hcde
 
-const url_base = 'https://www.washington.edu/students/timeschd/';
+(function() {
+    window.onload = function() {
+        fetch_ts('SUM', '2015', 'info');
+    };
 
-function loadXMLDoc(qtr, year, dept) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            document.getElementById("text").innerHTML = xmlhttp.responseText;
-            document.getElementById("type").innerHTML = xmlhttp.responseType;
-            document.getElementById("xml").innerHTML = xmlhttp.responseXML;
-        } else
-            console.log("Request failed :\\\\\\");
+    const url_base = 'https://www.washington.edu/students/timeschd/';
+
+    function fetch_ts(qtr, year, dept) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                document.getElementById("text").innerHTML = xmlhttp.responseText;
+                document.getElementById("type").innerHTML = xmlhttp.responseType;
+                document.getElementById("xml").innerHTML = xmlhttp.responseXML;
+            } else
+                console.log("Request failed :\\\\\\");
+        }
+        xmlhttp.open("GET", url_base + qtr + year + '/' + dept'.html', true);
+        xmlhttp.send();
     }
-    xmlhttp.open("GET", url_base + qtr + year + '/' + dept'.html', true);
-    xmlhttp.send();
-}
+})();
