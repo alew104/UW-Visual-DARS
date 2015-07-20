fdf0f017765440cb9bbda1c9b00c6a4ed988e900
Cechi Shi <shicechi@gmail.com>
2015-07-19 22:26:37 -0700

    Fixed syntax error

--- js/controller.js
+++ js/controller.js
@@ -1,45 +1,45 @@
 var myCtrl = myApp.controller('myCtrl', function($scope) {
 
-	$scope.data = JSON.parse(classes);
+    $scope.data = JSON.parse(classes);
 
     //$scope.netid = netid;
-	$scope.credits = 0;
+    $scope.credits = 0;
     $scope.electivesCredits = 0;
-	$scope.classesTaken = [];
+    $scope.classesTaken = [];
     $scope.electives = [];
 
-	$scope.markCompleted = function (_class) {
-		$scope.data.classes[_class].completed = true;
-		$scope.classesTaken.push(_class);
-        if(!$scope.data.classes[_class].required) {
+    $scope.markCompleted = function (_class) {
+        $scope.data.classes[_class].completed = true;
+        $scope.classesTaken.push(_class);
+        if (!$scope.data.classes[_class].required) {
             $scope.electives.push(_class);
             $scope.electivesCredits += $scope.data.classes[_class].credits;
         }
-		$scope.credits += $scope.data.classes[_class].credits;
-	};
+        $scope.credits += $scope.data.classes[_class].credits;
+    };
 
-	$scope.checkPrereq = function (_class) {
-		var result = true;
-		var prereqs = $scope.data.classes[_class].prereqs;
-		if (!prereqs) {
-			for (var i = 0; i < prereqs.length; i++) {
-				if (result) {
-					result = prereqs[i].completed;
-				}
-			}
-		}
-		return result;
-	};
+    $scope.checkPrereq = function (_class) {
+        var result = true;
+        var prereqs = $scope.data.classes[_class].prereqs;
+        if (!prereqs) {
+            for (var i = 0; i < prereqs.length; i++) {
+                if (result) {
+                    result = prereqs[i].completed;
+                }
+            }
+        }
+        return result;
+    };
 
-	$scope.suggestNextClass = function () {
+    $scope.suggestNextClass = function () {
 
-	};
+    };
 
     $scope.checkElectives = function () {
         return $scope.electivesCredits + '/75 credits';
     };
 
-	$scope.checkCredit = function () {
-		return $scope.credits + '/180 credits';
-	};
-};
+    $scope.checkCredit = function () {
+        return $scope.credits + '/180 credits';
+    };
+});
