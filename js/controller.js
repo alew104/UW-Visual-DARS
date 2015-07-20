d108f437fe71eee2f487fc396f01f6b07f508952
Alex Lew <alew104@gmail.com>
2015-07-19 20:40:11 -0700

    added orderBy

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,17 @@
+myApp.filter('orderObjectBy', function() {
+    return function(items, field, reverse) {
+        var filtered = [];
+        angular.forEach(items, function(item) {
+            filtered.push(item);
+        });
+        filtered.sort(function (a, b) {
+            return (a[field] > b[field] ? 1 : -1);
+        });
+        if(reverse) filtered.reverse();
+            return filtered;
+    };
+});
+
 var myCtrl = myApp.controller('myCtrl', function($scope) {
 
 	$scope.data = JSON.parse(classes);
