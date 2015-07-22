814cc46d6934aa6a0d03e0e3c694cbcc000884bc
Josh Nazarian <jknaz@cs.washington.edu>
2015-07-21 17:53:28 -0700

    put .js file in js folder

--- jtest/fetch_google.js
+++
@@ -1,5 +0,0 @@
-
-
-$.getJSON('http://whateverorigin.org/get?url=' + encodeURIComponent('http://www.washington.edu/students/timeschd/SUM2015/info.html') + '&callback=?', function(data){
-	$('body').xml(data.contents);
-});
